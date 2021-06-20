fb605c4a1c0fb2821a1eaddaec74869d4026511a
Allan Ramos <allangrds@gmail.com>
2021-06-20 13:11:01 -0300

    fix(jest): removing stories from coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/stories.tsx',
+    '!src/**/.stories.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
